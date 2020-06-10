2e47743a4eeec74e15fd3b5c5b2734b148d53770
unknown <tskim1995@naver.com>
2020-06-10 13:49:23 +0900

    Updates

---
+++ precache-manifest.271809bb01caa349dcaddad30557c39d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12642522e6e8be813fde343fc04213dd",
+    "url": "/asd123/index.html"
+  },
+  {
+    "revision": "0945bff5dae12cc6adb8",
+    "url": "/asd123/static/css/main.a249fabe.chunk.css"
+  },
+  {
+    "revision": "6415baf5b8864aac5dd1",
+    "url": "/asd123/static/js/2.2fb10e5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/asd123/static/js/2.2fb10e5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0945bff5dae12cc6adb8",
+    "url": "/asd123/static/js/main.bce9671e.chunk.js"
+  },
+  {
+    "revision": "fcb1a04fd1d02b3de22e",
+    "url": "/asd123/static/js/runtime-main.e58522e8.js"
+  }
+]);
